775c2dba318f2a497e71fdff2ed9d40bd4035b49
srishti-meesho <srishti.gupta@meesho.com>
2024-06-13 10:40:09 +0530

    refactored code

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,9 @@
-export const DATA_IDENTIFIER = "data-identifier";
+export const DATA_IDENTIFIER = 'data-identifier';
+
+export const ERROR = {
+    TEXT_TO_SPEECH_CONVERSION: 'Error converting text to speech',
+    AUTOPLAY_AUDIO: 'Audio autoplay failed.',
+    PLAY_AUDIO: 'Error playing audio',
+    ELEMENT_NOT_FOUND: 'Element not found',
+    NON_SUPPORTED_ACTION: 'Unsupported action type',
+};
